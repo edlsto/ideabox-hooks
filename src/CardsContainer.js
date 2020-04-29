7598cf6d91d1deba779fb065019be54db6d9871c
Edward Stoner <edwardlstoner@gmail.com>
2020-04-29 12:37:06 -0600

    Create CardsContainer

---
+++ src/CardsContainer.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./CardsContainer.css";
+import { connect } from "react-redux";
+import Card from "./Card";
+
+const CardsContainer = ({ ideas }) => {
+  const cards = ideas.map((idea) => {
+    return <Card title={idea.title} description={idea.description} />;
+  });
+  return <div>{cards}</div>;
+};
+
+const mapStateToProps = ({ ideas }) => ({
+  ideas,
+});
+
+export default connect(mapStateToProps)(CardsContainer);
